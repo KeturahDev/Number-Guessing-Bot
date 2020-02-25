35d92ff86741168954b8c99f4af2cee22ed65d64
Eric Settels and Keturah Howard <eric+keturah@>
2020-02-25 13:29:28 -0800

    do the same functionality with a recursive method

--- LowerHgiher.cs
+++ LowerHgiher.cs
@@ -4,6 +4,26 @@ using System.Collections.Generic;
 
 public class LowHigh
 {
+  public static void HighLow(int min, int max)
+    {
+      var rand = new Random();
+      int numb = rand.Next(min, max);
+      Console.WriteLine($"{numb}... is this your number?");
+      string response = Console.ReadLine();
+      if (response == "lower") 
+      {
+      HighLow(min,numb);
+      } 
+      else if (response == "higher")
+      {
+       HighLow(numb,max);
+      }
+      else
+      {
+        Console.WriteLine("Got it Dumb Human!");
+      }
+    }
+
   public static void Main()
   {
     Console.WriteLine("Pick a number, and Ill guess it.");
@@ -11,29 +31,30 @@ public class LowHigh
 
     int max= 100;
     int min= 0;
+    HighLow(min,max);
 
-    var rand = new Random();
-
-    for (int i = 1; i > 0; i++)
-    {
-      int numb = rand.Next(min, max);
-      Console.WriteLine($"{numb}... is this your number?");
-      Console.WriteLine("type correct, lower, or higher");
-      string response = Console.ReadLine();
-      if (response == "lower") 
-      {
-        max = numb;
-      } 
-      else if (response == "higher")
-      {
-        min = numb + 1;
-      }
-      else
-      {
-        Console.WriteLine("Ha! I Win!");
-        break;
-      }
     }
-  }
+
+    // for (int i = 1; i > 0; i++)
+    // {
+    //   int numb = rand.Next(min, max);
+    //   Console.WriteLine($"{numb}... is this your number?");
+    //   Console.WriteLine("type correct, lower, or higher");
+    //   string response = Console.ReadLine();
+    //   if (response == "lower") 
+    //   {
+    //     max = numb;
+    //   } 
+    //   else if (response == "higher")
+    //   {
+    //     min = numb + 1;
+    //   }
+    //   else
+    //   {
+    //     Console.WriteLine("Ha! I Win!");
+    //     break;
+    //   }
+    // }
+  
 
 }
